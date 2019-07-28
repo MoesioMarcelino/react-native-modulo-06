96fb69eedc40d01ec3c21925ea20ebc3574d88c8
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-07-28 20:02:14 -0300

    navigation pages

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,10 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+export default function User({ navigation }) {
   return <View />;
 }
+
+User.navigationOptions = {
+  title: 'Moesio',
+};
