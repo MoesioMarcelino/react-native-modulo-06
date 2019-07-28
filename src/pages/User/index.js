2f7db8177911b74b347395b3eabd333e6c126825
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-07-28 20:14:10 -0300

    search data into api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-  return <View />;
-}
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-User.navigationOptions = {
-  title: 'Moesio',
-};
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/repos`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return <View />;
+  }
+}
